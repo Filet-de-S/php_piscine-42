a7a044ead905526aa666a24c98e3dae039330e5b
sss <asd@asd.com>
2019-10-11 17:34:43 +0300

    1

--- d03/ex06/member_only.php
+++ d03/ex06/member_only.php
@@ -1,22 +1,21 @@
 <?php
 $a = $_SERVER;
+
 header("WWW-Authenticate: Basic realm=''Member area''");
-if ($_SERVER[PHP_AUTH_USER] == "zaz" && $_SERVER[PHP_AUTH_PW] == "Ilovemylittleponey")
+
+$login = base64_encode("zaz:Ilovemylittleponey");
+
+if ($_SERVER[HTTP_AUTHORIZATION] == "Basic {$login}")
 {
-    ?>
-<html><body>
-Hello Zaz<br />
-<img src='data:image/png,base64,<?php echo base64_encode(file_get_contents('../img/42.png')); ?>>
-</body></html>
-<?php
+    $imag = '../img/42.png';
+    $imag = base64_encode(file_get_contents($imag));
+    header('HTTP/1.0 200 Authorized');
+    echo "<html><body>\nHello Zaz<br />\n<img src='data:image/png;base64,{$imag}'\n</body></html>\n";
 }
 else
     {
-        header('HTTP/1.0 401 Unauthorized');
-        header('Date: Tue, 26 Mar 2013 09:42:42 GMT');
-        header('Server: Apache');
-        header('Content-type: text/html');
-
+    header('HTTP/1.0 401 Unauthorized');
+        
 ?>
 <html><body>That area is accessible for members only</body></html>
 <?php
